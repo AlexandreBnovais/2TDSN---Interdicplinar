d0ed78c31ada442f8f9a1499d586cd3984a15ffa
AlexandreBnovais <157971881+AlexandreBnovais@users.noreply.github.com>
2025-06-19 10:19:35 -0300

    Ajustando estrutura do codigo

--- App/backend/config/config.js
+++ App/backend/config/config.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'proUnitDB',
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
     connectionLimit: 10,
     waitForConnections: 1
 });
